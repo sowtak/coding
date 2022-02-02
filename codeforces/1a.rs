75aa269f1ac1838ee98a1b21d57bc23a0f095267
Sowa Takayanagi <55987516+tkyngs@users.noreply.github.com>
2022-02-02 20:10:47 +0900

    Add rust template

---
+++ codeforces/1a.rs
@@ -0,0 +1,12 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_white
+    };
+}
+
+fn run() {
+    input! {
+        t: usize,
+        ask: [u32; t]
+    }
+}
